54e2c7ca8adc404a2dbe37a5147997104fe3b320
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2025-02-04 08:16:50 +0800

    Update dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,16 @@
 import "module-alias/register";
 import { config } from "dotenv";
 import { Bot } from "@core/Bot";
+import {
+    DroidAPIRequestBuilder,
+    OsuAPIRequestBuilder,
+} from "@rian8337/osu-base";
 process.env.UV_THREADPOOL_SIZE = "128";
 
 config();
+
+DroidAPIRequestBuilder.setAPIKey(process.env.DROID_API_KEY!);
+OsuAPIRequestBuilder.setAPIKey(process.env.OSU_API_KEY!);
 
 const bot = new Bot();
 
