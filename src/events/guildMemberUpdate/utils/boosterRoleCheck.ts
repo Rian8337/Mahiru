8e7a7c669dcfd40a7e1b8218cdda582544900de0
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-06-20 23:09:16 +0800

    Send thank you message for boosts

--- src/events/guildMemberUpdate/utils/boosterRoleCheck.ts
+++ src/events/guildMemberUpdate/utils/boosterRoleCheck.ts
@@ -1,33 +1,53 @@
+import { Constants } from "@core/Constants";
 import { DatabaseManager } from "@database/DatabaseManager";
 import { EventUtil } from "@structures/core/EventUtil";
-import { GuildMember } from "discord.js";
+import { chatInputApplicationCommandMention, GuildMember } from "discord.js";
 
 export const run: EventUtil["run"] = async (
     _,
     oldMember: GuildMember,
     newMember: GuildMember,
 ) => {
-    if (!oldMember.premiumSince || newMember.premiumSince) {
-        return;
-    }
+    if (!oldMember.premiumSince && newMember.premiumSince) {
+        // User has started boosting the server, greet with thank you message if they can claim a booster role.
+        const generalChannel = await newMember.guild.channels.fetch(
+            Constants.mainServer,
+        );
 
-    const boosterRole =
-        await DatabaseManager.aliceDb.collections.boosterRole.getFromDiscordId(
+        if (!generalChannel?.isTextBased()) {
+            return;
+        }
+
+        const boosterRoleCount =
+            await DatabaseManager.aliceDb.collections.boosterRole.getCount();
+
+        if (boosterRoleCount >= 20) {
+            return;
+        }
+
+        await generalChannel.send({
+            content: `Thank you for boosting the server, ${newMember}! You can claim your booster role by using the ${chatInputApplicationCommandMention("booster", "claim", "1517880635702907021")} command.`,
+        });
+    } else if (oldMember.premiumSince && !newMember.premiumSince) {
+        // User has stopped boosting the server, proceed with booster role removal.
+        const boosterRole =
+            await DatabaseManager.aliceDb.collections.boosterRole.getFromDiscordId(
+                newMember.id,
+            );
+
+        if (!boosterRole) {
+            return;
+        }
+
+        await DatabaseManager.aliceDb.collections.boosterRole.deleteFromDiscordId(
             newMember.id,
         );
 
-    if (!boosterRole) {
-        return;
+        await newMember.guild.roles.delete(
+            boosterRole.roleId,
+            "Booster role removed due to user no longer boosting.",
+        );
     }
-
-    await DatabaseManager.aliceDb.collections.boosterRole.deleteFromDiscordId(
-        newMember.id,
-    );
-
-    await newMember.guild.roles.delete(
-        boosterRole.roleId,
-        "Booster role removed due to user no longer boosting.",
-    );
 };
 
 export const config: EventUtil["config"] = {
