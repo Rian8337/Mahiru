c50d6fc871a29eb4247b400cfabe0fabf9ab411e
Rian8337 <52914632+Rian8337@users.noreply.github.com>
2026-08-20 17:15:47 +0800

    Add missing `booster` autocomplete handler

---
+++ src/interactions/autocomplete/Tools/booster/booster.ts
@@ -0,0 +1,26 @@
+import { AutocompleteHandler } from "@structures/core/AutocompleteHandler";
+
+export const run: AutocompleteHandler["run"] = async (_, interaction) => {
+    if (!interaction.inCachedGuild()) {
+        return;
+    }
+
+    const focused = interaction.options.getFocused(true);
+
+    if (focused.name === "emoji") {
+        interaction.respond(
+            interaction.guild.emojis.cache
+                .filter((emoji) =>
+                    emoji.name?.toLowerCase().includes(focused.value.toLowerCase())
+                )
+                .map((emoji) => ({
+                    name: emoji.name,
+                    value: emoji.id,
+                }))
+        );
+    }
+};
+
+export const config: AutocompleteHandler["config"] = {
+    name: "recalc",
+};
